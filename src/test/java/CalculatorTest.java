77fd30ee4609c2faaac098a5ea1d4ece43eacba3
Gilmar Bernardes <galtran.void@gmail.com>
2023-08-22 20:21:17 +0000

    creating tests for substract, multiply and divide methods of Calculator class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorTest {
 
@@ -15,4 +16,40 @@ public class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testSubtract() {
+        int expected = 0;
+
+        int actualPositive = calculator.subtract(2, 1);
+        int actualNegative = calculator.subtract(1, 2);
+        int actualZero = calculator.subtract(2, 2);
+
+        // result must be positive
+        assertTrue(expected > actualNegative);
+
+        // result must be negative
+        assertTrue(expected < actualPositive);
+
+        // result must be zero
+        assertEquals(expected, actualZero);
+    }
+
+    @Test
+    public void testMultiply() {
+        int expected = 10;
+
+        int actual = calculator.multiply(5, 2);
+        
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testDivide() {
+        int expected = 5;
+
+        int actual = calculator.divide(10, 2);
+
+        assertEquals(expected, actual);
+    }
 }
