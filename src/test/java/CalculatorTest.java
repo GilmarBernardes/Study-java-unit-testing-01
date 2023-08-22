90567274929e6aea698b06685334d52cef620a83
Gilmar Bernardes <galtran.void@gmail.com>
2023-08-22 22:35:28 +0000

    created a parameterizedTest for isEven method on Calculator class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -42,14 +44,15 @@ public class CalculatorTest {
         assertEquals(expected, actual);
     }
 
-    @Test
-    public void testIsEven() {
-        boolean actual = calculator.isEven(2);
+    @ParameterizedTest
+    @ValueSource(ints = {2, -2, -100000, 200000})
+    public void testIsEven(int number) {
+        boolean actual = calculator.isEven(number);
 
         assertTrue(actual);
     }
 
-    @Test
+   @Test
     public void testIsOdd() {
         boolean actual = calculator.isEven(3);
 
